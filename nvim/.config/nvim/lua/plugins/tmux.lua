4888eaf530da9dc17f56c72e298e704769a8853c
trappedintesseract <sparsh.secret@gmail.com>
2026-08-10 03:02:08 +0530

    Removed duplicate keymappings

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -5,26 +5,6 @@ return {
 
         config = function()
             vim.g.tmux_navigator_no_mappings = 1
-
-            vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", {
-                silent = true,
-                desc = "Navigate left",
-            })
-
-            vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", {
-                silent = true,
-                desc = "Navigate down",
-            })
-
-            vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", {
-                silent = true,
-                desc = "Navigate up",
-            })
-
-            vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", {
-                silent = true,
-                desc = "Navigate right",
-            })
         end,
     },
 }
